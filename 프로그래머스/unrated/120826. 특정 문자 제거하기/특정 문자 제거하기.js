a3320ce57fc05ebfb27cbc39dfdeb1593b050980
mgm9458 <147527431+mgm9458@users.noreply.github.com>
2023-10-27 14:16:57 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -6,4 +6,13 @@ function solution(my_string, letter) {
          }
      }
   return answer;
+    //let answer = [...my_string].filter((el) => !el.includes(letter))
+    //return answer.join('')
+    // let answer = ''
+    // for(let i = 0; i < my_string.length; i++){
+    //     if(!my_string[i].includes(letter)){
+    //         answer += my_string[i]
+    //     }
+    // }
+    // return answer;
 }
