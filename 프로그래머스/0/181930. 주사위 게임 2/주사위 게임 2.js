16c55ae7965daada916899e20c732393251cf983
Minyoung <147527431+mgm9458@users.noreply.github.com>
2024-03-07 18:55:51 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,16 @@
+function pow2(a, b, c) {
+  return Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2);
+}
+function pow3(a, b, c) {
+  return Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3);
+}
+
+function solution(a, b, c) {
+  if (a === b && b === c) {
+    return (a + b + c) * pow2(a, b, c) * pow3(a, b, c);
+  } else if (a !== b && b !== c && a !== c) {
+    return a + b + c;
+  } else {
+    return (a + b + c) * pow2(a, b, c);
+  }
+}
